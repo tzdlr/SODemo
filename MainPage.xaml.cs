45c56f1bca3168ecc2b3b8e10b692632d83dc838
TinoZ <44774647+tzdlr@users.noreply.github.com>
2021-08-23 21:49:35 +0200

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace UWPTest
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<ListModel> MyList = new ObservableCollection<ListModel>();
+
+
+        public bool EditMode
+        {
+            get { return (bool)GetValue(EditViewProperty); }
+            set
+            {
+                StaticHelper.EditView = value;
+                Task.Run(() => BuildSource());
+                SetValue(EditViewProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for EditView.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EditViewProperty =
+            DependencyProperty.Register("EditView", typeof(bool), typeof(MainPage), new PropertyMetadata(false));
+
+        private DataService _ds = new DataService();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _ds.LoadData();
+            BuildSource();
+        }
+
+        private async void BuildSource()
+        {
+            var list = _ds.getCachedEntries();
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                 MyList.Clear();
+                 foreach (var i in list)
+                 {
+                     MyList.Add(i);
+                 }
+            });
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPTest
+{
+    public class Model
+    {
+        public string Text;
+
+        public Model(string t) { Text = t; }
+    }
+
+    public class ListModel
+    {
+        public List<Model> ModelList = new List<Model>();
+    }
+
+    public class ModelSelector : DataTemplateSelector
+    {
+        public DataTemplate ViewTemplate { get; set; }
+        public DataTemplate EditTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            return StaticHelper.EditView ? EditTemplate : ViewTemplate;
+        }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return StaticHelper.EditView ? EditTemplate : ViewTemplate;
+        }
+    }
+    static class StaticHelper
+    {
+        public static bool EditView { get; set; } = false;
+    }
+
+    public class DataService
+    {
+        private List<ListModel> _cache = new List<ListModel>();
+
+        public void LoadData()
+        {
+            _cache.Clear();
+            for (var i = 0; i < 5; i++)
+            {
+                ListModel nl = new ListModel();
+                for (var k = 0; k < 10; k++)
+                {
+                    nl.ModelList.Add(new Model(i + "-" + k));
+                }
+                _cache.Add(nl);
+            }
+        }
+
+        public List<ListModel> getCachedEntries()
+        {
+            return _cache;
+        }
+    }
+}
